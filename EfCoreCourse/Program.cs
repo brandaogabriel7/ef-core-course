d685a91f6bad7be4c66d1dae4898de5f4cb7a2a8
Gabriel Brandão <gabrielsb@take.net>
2020-09-26 16:58:06 -0300

    feat: Initialize Solution with Console app

---
+++ EfCoreCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCoreCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
