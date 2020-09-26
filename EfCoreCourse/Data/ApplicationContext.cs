d1819d440d9a3a12fceef6a7e8458e339ce5c4f6
Gabriel Brandão <gabrielsb@take.net>
2020-09-26 17:47:26 -0300

    feat: Add ApplicationContext.cs

---
+++ EfCoreCourse/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreCourse.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private const string ConnectionString = "Host=localhost;Port:5432;Pooling=true;Database=ef_core_course;User ID=admin;Password=admin";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+    }
+}
