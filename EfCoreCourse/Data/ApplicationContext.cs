96299911ed8241dbadcd695da75b86c642afa8bd
Gabriel Brandão <gabrielsb@take.net>
2020-09-28 12:49:29 -0300

    feat: Add logging and create insert and select methods

--- EfCoreCourse/Data/ApplicationContext.cs
+++ EfCoreCourse/Data/ApplicationContext.cs
@@ -1,14 +1,23 @@
 using EfCoreCourse.Data.Configurations;
+using EfCoreCourse.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EfCoreCourse.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
         private const string ConnectionString = "Host=localhost;Port=5432;Pooling=true;Database=ef_core_course;User ID=admin;Password=admin";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(ConnectionString);
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseNpgsql(ConnectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- EfCoreCourse/Program.cs
+++ EfCoreCourse/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 using EfCoreCourse.Data;
+using EfCoreCourse.Domain;
+using EfCoreCourse.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace EfCoreCourse
@@ -9,14 +11,75 @@ namespace EfCoreCourse
     {
         static void Main(string[] args)
         {
+            // InserirDados();
+            // InserirDadosEmMassa();
+            ConsultarDados();
+        }
+
+        private static void ConsultarDados()
+        {
             using var dbContext = new ApplicationContext();
 
-            var exists = dbContext.Database.GetPendingMigrations().Any();
-            if (exists)
+            // var consultaPorSintaxe = (from c in dbContext.Clientes where c.Id > 0 select c).ToList();
+            // Se tivesse sido feita com AsNoTracking(), não guardaria cópia em memória, portanto o Find teria que buscar no bd
+            var consultaPorMetodo = dbContext.Clientes.Where(c => c.Id > default(int))
+                .OrderBy(c => c.Id)
+                .ToList();
+            foreach (var cliente in consultaPorMetodo)
             {
-                Console.WriteLine("There are pending Migrations.");
+                Console.WriteLine($"Consultando cliente: {cliente.Id}");
+                // Procura primeiro em memória
+                // dbContext.Clientes.Find(cliente.Id);
+                dbContext.Clientes.FirstOrDefault(c => c.Id == cliente.Id);
             }
-            Console.WriteLine("Hello World!");
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Eu mesmo",
+                CEP = "34685974",
+                Cidade = "Betim",
+                Estado = "MG",
+                Telefone = "31999999999"
+            };
+
+            using var dbContext = new ApplicationContext();
+            dbContext.AddRange(produto, cliente);
+
+            var registros = dbContext.SaveChanges();
+            Console.WriteLine(registros);
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var dbContext = new ApplicationContext();
+            dbContext.Produtos.Add(produto);
+            dbContext.Set<Produto>().Add(produto);
+            dbContext.Entry(produto).State = EntityState.Added;
+            dbContext.Add(produto);
+
+            var registros = dbContext.SaveChanges();
+            Console.WriteLine(registros);
         }
     }
 }
